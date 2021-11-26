74a566f7ad4dd124bf56af1b2c107d6ddf1ff2d4
callMyTherapist <85406726+callMyTherapist@users.noreply.github.com>
2021-11-26 15:11:24 -0600

    Add files via upload

--- js/site.js
+++ js/site.js
@@ -17,6 +17,13 @@ $(document).ready(function () {
             "/": '&#x2F;'
         }
 
+    var details1 = document.getElementById("details1")
+    var details2 = document.getElementById("details2")
+    var details3 = document.getElementById("details3")
+    var details4 = document.getElementById("details4")
+    var smallName = document.getElementById("smallName")
+
+
     function init() {
         $window.on('scroll', onScroll)
         $window.on('resize', resize)
@@ -24,6 +31,12 @@ $(document).ready(function () {
         $document.on('click', closePopover)
         $('a[href^="#"]').on('click', smoothScroll)
         buildSnippets();
+
+        var element = document.createElement("details");
+        var isSupported = element.constructor !== window.HTMLUnknownElement && element.constructor !== window.HTMLElement
+        if (isSupported == false) { document.getElementById("divMenu1").style.display = 'none';}
+
+        onScroll();
     }
 
     function smoothScroll(e) {
@@ -67,6 +80,20 @@ $(document).ready(function () {
     }
 
     function onScroll() {
+
+        var w = 0
+        if (window.outerHeight) { w = window.outerWidth } else { w = document.body.clientWidth }
+        console.log(w)
+        
+        if (w < 1250) { details4.style.display = 'none' } else { details4.style.display = '' }
+        if (w < 1170) { details3.style.display = 'none' } else { details3.style.display = '' }
+        if (w < 1000) { details2.style.display = 'none' } else { details2.style.display = '' }
+
+        $body.addClass('has-docked-nav')
+        ShowLogo()
+
+        return
+
         if (navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
             $body.addClass('has-docked-nav')
             ShowLogo()
@@ -90,6 +117,28 @@ $(document).ready(function () {
         })
     }
 
+    var Overlaps = (function () {
+        function getPositions(elem) {
+            var pos, width, height;
+            pos = $(elem).position();
+            width = $(elem).width() / 2;
+            height = $(elem).height();
+            return [[pos.left, pos.left + width], [pos.top, pos.top + height]];
+        }
+
+        function comparePositions(p1, p2) {
+            var r1, r2;
+            r1 = p1[0] < p2[0] ? p1 : p2;
+            r2 = p1[0] < p2[0] ? p2 : p1;
+            return r1[1] > r2[0] || r1[0] === r2[0];
+        }
+
+        return function (a, b) {
+            var pos1 = getPositions(a),
+                pos2 = getPositions(b);
+            return comparePositions(pos1[0], pos2[0]) && comparePositions(pos1[1], pos2[1]);
+        };
+    })();
 
     init();
 
